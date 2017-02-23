c1ef3bec1f4ae82212e7372e2427b5f279c2ed80
codemeleon <anmol.kiran@gmail.com>
2017-02-23 11:46:20 +0200

    Allteration for whole folder conversion

--- src/aa2codon.py
+++ src/aa2codon.py
@@ -1,10 +1,13 @@
+#! /usr/bin/env python
+"""Protein to Codon."""
 import click
-from os import path
+from glob import glob
+from os import path, makedirs
 from Bio import SeqIO
 
 
 def amino2codon(aa, nuc):
-    "Convert to Codon."
+    """Convert to Codon."""
     codon = ''
     n = 0
     for a in aa:
@@ -17,36 +20,41 @@ def amino2codon(aa, nuc):
 
 
 @click.command()
-@click.option("--aaalnfile", help="Amino acid alignment file",
+@click.option("--aaalnfold", help="Amino acid alignment folder",
               type=str, default=None, show_default=True)
-@click.option("--nucfile", help="Coding nucleotide folder",
+@click.option("--nucfold", help="Coding nucleotide folder",
               type=str, default=None, show_default=True)
-@click.option("--codonalnfile", help="Codon alignment output",
+@click.option("--cdsalnfold", help="Codon alignment output folder",
               type=str, default=None, show_default=True)
-def run(aaalnfile, nucfile, codonalnfile):
+@click.option("--ofilext", default="Output File extension",
+              type=str, default="cds", show_default=True)
+def run(aaalnfold, nucfold, cdsalnfold, ofilext):
     """Code generate codon alignment base on amino acid alignments."""
     """ corresponding nucleotide sequences."""
 
-    if aaalnfile is None or not path.isfile(aaalnfile):
-        click.echo("Amino acid alignemt file is not given or path doesn't\
-                   exist. Exiting ...")
-        exit(1)
-    elif nucfile is None or not path.isfile(nucfile):
-        click.echo("Nucleotide folder is not given or path doesn't exist."
-                   " Exiting ....")
-        exit(1)
-    elif not codonalnfile:
-        click.echo("Codon file path not given. Exiting ...")
-        exit(1)
+    assert aaalnfold, "Animo acid aligment folder not give. Exiting .."
+    assert nucfold, "DNA input folder not give. Exiting .."
+    assert cdsalnfold, "Codon Alignment output folder not give. Exiting .."
+    assert path.isdir(aaalnfold), "Given amino acid alignment folder doesn't\
+        exist. Exiting ..."
+    assert path.isdir(nucfold), "Given nucleotide folder doesn't\
+        exist. Exiting ..."
+    assert not makedirs(cdsalnfold, exist_ok=True), "Unable to create output\
+        folder. Please check permission. Exiting ..."
 
     nuc_seq = {}
-    for rec in SeqIO.parse(nucfile, 'fasta'):
-        nuc_seq[rec.id] = rec.seq
-    with open(codonalnfile, "w") as fout:
-        for rec in SeqIO.parse(aaalnfile, "fasta"):
-            fout.write(">%s\n%s\n" % (rec.id,
-                                      amino2codon(rec.seq,
-                                                  nuc_seq[rec.id])))
+    for fl in glob("%s/*" % nucfold):
+        for rec in SeqIO.parse(fl, 'fasta'):
+            nuc_seq[rec.id] = rec.seq
+
+    for fl in glob("%s/*" % aaalnfold):
+        file_id = path.split(fl)[1].split(".")[0]
+        with open("%s/%s.%s" % (cdsalnfold, file_id, ofilext), "w") as fout:
+            for rec in SeqIO.parse(fl, "fasta"):
+                fout.write(">%s\n%s\n" %
+                           (rec.id,
+                            amino2codon(rec.seq,
+                                        nuc_seq[rec.id])))
 
 
 if __name__ == '__main__':
