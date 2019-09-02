cda8ca9d46bea7bc97884c20e0cc4a18de5760be
codemeleon <anmol.kiran@gmail.com>
2019-09-02 15:52:28 +0200

    added addition configuration and libraries

--- src/conda_install.sh
+++ src/conda_install.sh
@@ -5,6 +5,8 @@
 conda config --add channels r
 conda config --add channels bioconda
 
+conda install -c anaconda gcc
+
 # Adding Tool and components
 
 conda install blast blat mafft muscle \
@@ -14,10 +16,23 @@ conda install blast blat mafft muscle \
 	sra-tools spades picard fastqc kraken kraken2 clustalw prokka \
 	samtools bcftools unicycler smalt \
 	biopython ruffus pysam \
-	roary \
+	roary ariba \
 	r r-irkernel
 
 conda install -c conda-forge nodejs
 
 
 pip install recipy ete3 recipy iva
+
+# conda create -n py365 python=3.6.5
+conda activate py365
+conda install ariba
+conda deactivate
+# added /.anmol/anaconda3/envs/py365/bin/ariba in /etc/bash.bashrc
+
+
+# conda create -n py27 python=2.7.5
+conda activate py27
+conda install srst2
+conda deactivate
+# added /.anmol/anaconda3/envs/py27/bin/srst2 in /etc/bash.bashrc
