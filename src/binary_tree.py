f62e170d3e6370bdb1c84f7cce2a153b980f56a9
codemeleon <anmol.kiran@gmail.com>
2020-02-27 16:13:43 +0200

    Script for binary tree for

---
+++ src/binary_tree.py
@@ -0,0 +1,44 @@
+import click
+from ete3 import Tree
+from os import path
+
+
+@click.command()
+@click.option("-i",
+        help="Input newick file having multiple leaves at same node",
+        type=str, default=None, show_default=True)
+@click.option("-o",
+        help="Ouput binary newick file",
+        type=str, default=None, show_default=True)
+def run(i,o):
+    """This script generates binary tree to generate tangle gram using R
+    package `dendextend`."""
+    if not i:
+        exit("Input file not given")
+    if not path.exists(i):
+        exit("Given input file path doesn't exist")
+    if not path.isfile(i):
+        exit("Given input path is not a file")
+    if not o:
+        exit("Output file path not given")
+    try:
+        tree = Tree(i)
+        for n in tree.traverse("preorder"):
+            if len(n.get_children()) >2 and not n.is_root():
+                leaf_names = n.get_leaf_names()
+                for leaf in leaf_names[1:]:
+                    dl = tree.search_nodes(name=leaf)[0]
+                    dl.delete()
+                tx = ""
+                for x_ in leaf_names[1:-1]:
+                    tx +=f"({x_}:0,"
+                tx +=f"{leaf_names[-1]}:0"+")"*(len(leaf_names)-2)+";"
+                tt = Tree(tx)
+                n.add_child(tt)
+        tree.write(outfile=o)
+    except:
+        print("Please check you input file format")
+
+
+if __name__ == '__main__':
+    run()
