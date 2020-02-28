2cf301fcc9762ebf06f5148a5753900b436a1916
codemeleon <anmol.kiran@gmail.com>
2020-02-28 05:31:56 +0200

    Added small lengths

--- src/binary_tree.py
+++ src/binary_tree.py
@@ -4,13 +4,21 @@ from os import path
 
 
 @click.command()
-@click.option("-i",
-        help="Input newick file having multiple leaves at same node",
-        type=str, default=None, show_default=True)
-@click.option("-o",
-        help="Ouput binary newick file",
-        type=str, default=None, show_default=True)
-def run(i,o):
+@click.option(
+    "-i",
+    help="Input newick file having multiple leaves at same node",
+    type=str,
+    default=None,
+    show_default=True,
+)
+@click.option(
+    "-o",
+    help="Ouput binary newick file",
+    type=str,
+    default=None,
+    show_default=True,
+)
+def run(i, o):
     """This script generates binary tree to generate tangle gram using R
     package `dendextend`."""
     if not i:
@@ -24,15 +32,19 @@ def run(i,o):
     try:
         tree = Tree(i)
         for n in tree.traverse("preorder"):
-            if len(n.get_children()) >2 and not n.is_root():
+            if len(n.get_children()) > 2 and not n.is_root():
                 leaf_names = n.get_leaf_names()
                 for leaf in leaf_names[1:]:
                     dl = tree.search_nodes(name=leaf)[0]
                     dl.delete()
                 tx = ""
                 for x_ in leaf_names[1:-1]:
-                    tx +=f"({x_}:0,"
-                tx +=f"{leaf_names[-1]}:0"+")"*(len(leaf_names)-2)+";"
+                    tx += f"({x_}:1e-9,"
+                tx += (
+                    f"{leaf_names[-1]}:1e-9"
+                    + ")" * (len(leaf_names) - 2)
+                    + ";"
+                )
                 tt = Tree(tx)
                 n.add_child(tt)
         tree.write(outfile=o)
@@ -40,5 +52,5 @@ def run(i,o):
         print("Please check you input file format")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()
