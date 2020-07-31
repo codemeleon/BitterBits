9dff2f95d6e353146e849a849a47e3023f8fcba5
codemeleon <anmol.kiran@gmail.com>
2020-07-31 18:32:00 +0200

    autoplotter added

--- src/python_packages.sh
+++ src/python_packages.sh
@@ -12,11 +12,15 @@ pip install ete3 \
 	cufflinks \
 	torch \
 	# Stats
-	pingouin
+	pingouin \
+	tableone \
+	scikit-posthocs \
+	statannot \
+	ipython-autoimport \
+	autoplotter
 
 # https://raw.githubusercontent.com/myrthings/catscatter/master/catscatter.py
 
 # qgrid
 jupyter nbextension enable --py --sys-prefix widgetsnbextension
 jupyter labextension install qgrid2
-
