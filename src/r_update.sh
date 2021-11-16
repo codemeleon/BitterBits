c0d3c95bbebe23a47f7fa0f1916eda7681d24ac3
codemeleon <anmol.kiran@gmail.com>
2021-11-16 09:40:32 +0200

    Bioconductior package updater

--- src/r_update.sh
+++ src/r_update.sh
@@ -1,1 +1,3 @@
 echo "update.packages(repos=\"https://cran.rstudio.com\", ask=FALSE)" | R --no-save
+echo "BiocManager::install(ask=FALSE)" | R --no-save
+echo "If update failed, try to turn off anaconda environment"
