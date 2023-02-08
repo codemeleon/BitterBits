37f9f6bd411974668146e39a7bed8ca4ba5334d9
codemeleon <anmol.kiran@gmail.com>
2023-02-08 18:23:16 +0000

    Code correction

--- src/python_packages.sh
+++ src/python_packages.sh
@@ -1,57 +1,59 @@
 pip install ete3 \
-    pandas \
-    geopy \
-    pyclustering \
-    biopython \
-    scikit-learn \
-    scikit-bio \
-    statsmodels \
-    scipy \
-    numpy \
-    qgrid \
-    catscatter \
-    plotly \
-    django-stubs \
-    pandas-stubs \
-    cufflinks \
-    dataprep dtale autoviz \
-    torch \
-    jbrowse-jupyter \
-    gffutils \
-    ueberzug cairosvg pnglatex kaleido \
-    thefuck \
-    ploomber \ # Automation
-docrepr jupyterlab_pygments \
-    evidently deepchecks tensorflow-data-validation # For deep learning data validation
+	pandas \
+	geopy \
+	pyclustering \
+	biopython \
+	scikit-learn \
+	scikit-bio \
+	statsmodels \
+	scipy \
+	numpy \
+	qgrid \
+	catscatter \
+	plotly \
+	django-stubs \
+	pandas-stubs \
+	cufflinks \
+	dataprep dtale autoviz \
+	torch \
+	pynvim \
+	jbrowse-jupyter \
+	gffutils \
+	ueberzug cairosvg pnglatex kaleido \
+	thefuck \
+	ploomber \
+	docrepr \
+	jupyterlab_pygments \
+	evidently deepchecks tensorflow-data-validation # For deep learning data validation
 
 # For automated help string, you must execute in first shell of notebook
 # https://blog.jupyter.org/inspector-jupyterlab-404cce3e1df6
 
 # Stats
 pingouin \
-    tableone \
-    scikit-posthocs \
-    statannot \
-    visidata \
-    ipython-autoimport \
-    black \
-    autoplotter \
-    pycaret \
-    openrefine \
-    pandasgui \
-    visidata \
-    sweetviz \
-    voila-gridstack \
-    pyjanitor \
-    nbterm \
-    word2number \
-    nafas \
-    pythran \
-    metapub \
-    biocode \
-    PyPaperBot \
-    jedi-language-server \
-    mitoinstaller
+	tableone \
+	scikit-posthocs \
+	statannot \
+	visidata \
+	ipython-autoimport \
+	black \
+	autoplotter \
+	pycaret \
+	openrefine \
+	pandasgui \
+	visidata \
+	sweetviz \
+	voila-gridstack \
+	pyjanitor \
+	nbterm \
+	word2number \
+	nafas \
+	pythran \
+	metapub \
+	biocode \
+	PyPaperBot \
+	jedi-language-server \
+	mitoinstaller
 # https://raw.githubusercontent.com/myrthings/catscatter/master/catscatter.py
 # https://github.com/FePhyFoFum/PyPHLAWD
 
